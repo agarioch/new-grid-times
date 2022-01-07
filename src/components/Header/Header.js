41dd35a244ba0a4d4408519647bdfab660d2cf54
Alistair <alistairgarioch@gmail.com>
2022-01-07 18:34:21 +0000

    refactor: remove superheader and modify main header for laptop and larger screens

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <MainActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </MainActionGroup>
         <Logo />
+        <LoginGroup>
+          <Button>Subscribe</Button>
+          <SubscriberLink href="##">Already a subscriber?</SubscriberLink>
+        </LoginGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,9 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +80,35 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.laptopAndUp} {
+    align-items: baseline;
+    justify-content: space-between;
+  }
+`;
+
+const MainActionGroup = styled(ActionGroup)`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const LoginGroup = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscriberLink = styled.a`
+  color: ${COLORS.gray[900]};
+  font-size: ${14 / 16}rem;
+  font-style: italic;
+  text-decoration: underline;
 `;
 
 export default Header;
